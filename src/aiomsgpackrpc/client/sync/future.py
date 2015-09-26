34000a7fa5c7ea53a51ca9b2453bd9249751bf14
Mathieu Lecarme <mlecarme@bearstech.com>
2015-09-26 20:07:00 +0200

    conccurent.fture like API.

--- src/aiomsgpackrpc/client/sync/future.py
+++ src/aiomsgpackrpc/client/sync/future.py
@@ -10,22 +10,38 @@ class Server(object):
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._socket.connect((host, port))
         self.proxy = Proxy(self)
+        self._waiting = 0
+        self._responses = dict()
 
     def call(self, name, args):
         self._cpt += 1
         self._socket.sendall(packb([0, self._cpt, name, args]))
+        self._waiting += 1
         return self._cpt
 
     def close(self):
         self._socket.close()
 
-    def __iter__(self):
+    def responses(self):
         u = Unpacker()
-        while True:
+        while self._waiting > 0:
             data = self._socket.recv(2048)
             u.feed(data)
             for r in u:
-                yield r
+                if r[0] == 2:
+                    pass
+                else:
+                    self._waiting -= 1
+                    yield r
+
+
+def as_completed(fs):
+    s = fs[0]._server
+    for r in s.responses():
+        f = s._responses[r[1]]
+        f.set_result(r[3])
+        f.set_exception(r[2])
+        yield f
 
 
 class Method(object):
@@ -35,15 +51,46 @@ class Method(object):
         self._name = name
 
     def __call__(self, *args):
-        return Future(self._server.proxy, self._server.call(self._name, args))
+        f = Future(self._server, self._server.call(self._name, args))
+        self._server._responses[f._id] = f
+        return f
 
 
 class Future(object):
+    _done = False
+    _result = None
+    _exception = None
+    _cb = None
 
-    def __init__(self, proxy, id):
-        self._proxy = proxy
+    def __init__(self, server, id):
+        self._server = server
         self._id = id
 
+    def result(self):
+        if self._exception is not None:
+            raise self._exception
+        return self._result
+
+    def done(self):
+        return self._done
+
+    def add_done_callback(self, fn):
+        self._cb = fn
+
+    def set_result(self, result):
+        self._result = result
+        if not self._done and self._cb is not None:
+            self._cb(self)
+        self._done = True
+
+    def set_exception(self, exception):
+        self._exception = exception
+        if not self._done and self._cb is not None:
+            self._cb(self)
+        self._done = True
+
+    def __delete__(self):
+        del self._server._responses[self._id]
 
 class Proxy(object):
 
---
+++ tests/future.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.append('src')
+
+from aiomsgpackrpc.client.sync.future import Server, as_completed
+
+
+if __name__ == '__main__':
+    s = Server('localhost', 8888)
+    p = s.proxy
+    f1 = p.add(1, 2)
+    print("f1", f1)
+    f2 = p.add(1, 0)
+    print("f2", f2)
+    for r in as_completed([f1, f2]):
+        print(r.result())
