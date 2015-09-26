10b0b09b2001378d68229af1fa27de6e7871dbff
Mathieu Lecarme <mlecarme@bearstech.com>
2015-09-26 20:06:00 +0200

    Choose your Method implementation.

--- src/aiomsgpackrpc/client/sync/__init__.py
+++ src/aiomsgpackrpc/client/sync/__init__.py
@@ -2,28 +2,6 @@ import socket
 from Queue import Queue
 
 
-class Server(object):
-
-    def __init__(self, host, port, max_events=500):
-        self._cpt = -1
-        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._socket.connect((host, port))
-        self.proxy = Proxy(self)
-        self.events = Queue(maxsize=max_events)
-
-    def close(self):
-        self._socket.close()
-
-
-class Proxy(object):
-
-    def __init__(self, server):
-        self.__server = server
-
-    def __getattr__(self, name):
-        return Method(self.__server, name)
-
-
 class Method(object):
 
     def __init__(self, server, name):
@@ -35,3 +13,26 @@ class Method(object):
         if r[2] is not None:
             raise MsgPackRpcError(r[2])
         return r[3]
+
+
+class Server(object):
+
+    def __init__(self, host, port, max_events=500, method=Method):
+        self._cpt = -1
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.connect((host, port))
+        self.proxy = Proxy(self, method)
+        self.events = Queue(maxsize=max_events)
+
+    def close(self):
+        self._socket.close()
+
+
+class Proxy(object):
+
+    def __init__(self, server, method=Method):
+        self._server = server
+        self._method = method
+
+    def __getattr__(self, name):
+        return self._method(self._server, name)
