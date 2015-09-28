7d3a684ec794cd76d4cbf2ebf894711c06ead4a9
Mathieu Lecarme <mlecarme@bearstech.com>
2015-09-29 00:27:46 +0200

    Ugly aio client test.

---
+++ tests/aio_client.py
@@ -0,0 +1,19 @@
+import sys
+sys.path.append('src')
+
+import asyncio
+
+from aiomsgpackrpc.client.async import Server
+
+loop = asyncio.get_event_loop()
+s = Server('localhost', 8888, loop)
+loop.run_until_complete(s.start())
+f1 = s.proxy.add(1, 2)
+f2 = s.proxy.add(1, 0)
+print(f1)
+print(f2)
+loop.run_until_complete(asyncio.wait([f1, f2]))
+print(f1)
+print(f2)
+#loop.run_until_complete(s.stop())
+loop.close()
